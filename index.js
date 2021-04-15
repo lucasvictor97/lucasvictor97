955edef4359ea583602e0cc8ffe8ebe4199b2deb
lucasvictor97 <59706064+lucasvictor97@users.noreply.github.com>
2021-04-15 15:51:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var options =
 		username: "lucasvictor97",
 		password: "oauth:isc1hbr0c0n9n35b9ti1qr93ub2sz9",  
 	},
-	channels: ['#gaules', '#stereonline', '#deercheerup', '#StereOfflineX', '#gaguinhoo157', '#Dilera', '#skyzaooofps', '#umbrinquedo', '#velhovamp1', '#zigueira', '#pescocofino', '#murilo_rt', '#jeffaocs', '#csrfps', '#PlayHard', '#mibrtv', '#brnwowzk1', '#furiatv', '#skipnho',] //ADICIONA OS CANAIS AQ COM '#NOME'
+	channels: ['#gaules', '#stereonline', '#deercheerup', '#sr_thulium', '#StereOfflineX', '#gaguinhoo157', '#Dilera', '#skyzaooofps', '#umbrinquedo', '#velhovamp1', '#zigueira', '#pescocofino', '#murilo_rt', '#jeffaocs', '#csrfps', '#PlayHard', '#mibrtv', '#brnwowzk1', '#furiatv', '#skipnho',] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
 
 var client = new tmi.client(options);
